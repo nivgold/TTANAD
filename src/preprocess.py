73ea80d46b53eeac61af42b6d5a72af34ba2fa53
nivgold <nivgold@post.bgu.ac.il>
2023-01-16 19:20:34 +0200

    update preprocess to extract also prefixspan

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,14 +1,24 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
+from statsmodels.tsa.holtwinters import SimpleExpSmoothing
+from statsmodels.tsa.arima.model import ARIMA
 import argparse
+import pywt
+from prefixspan import PrefixSpan
+from pyts.approximation import SymbolicAggregateApproximation
 from tqdm import tqdm
+
+import warnings
+warnings.simplefilter("ignore", UserWarning)
 
 DATASET_DATA_FILE = {
     'ids17': {
         'data': 'ids2017_data.csv',
         'normal_class': 'BENIGN',
         'label_col': ' Label',
+        'time_col': ' Timestamp',
+        'prefixspan_col': 'Flow Bytes/s',
         'features': [' Flow Duration',
                             ' Total Fwd Packets', ' Total Backward Packets',
                             'Total Length of Fwd Packets', ' Total Length of Bwd Packets',
@@ -41,6 +51,8 @@ DATASET_DATA_FILE = {
         'data': 'ids2018_data.csv',
         'normal_class': 'Benign',
         'label_col': 'Label',
+        'time_col': 'Timestamp',
+        'prefixspan_col': 'Flow Byts/s',
         'features': ['Flow Duration', 'Tot Fwd Pkts',
                                'Tot Bwd Pkts', 'TotLen Fwd Pkts', 'TotLen Bwd Pkts', 'Fwd Pkt Len Max',
                                'Fwd Pkt Len Min', 'Fwd Pkt Len Mean', 'Fwd Pkt Len Std',
@@ -67,6 +79,7 @@ DATASET_DATA_FILE = {
         'data': 'unsw-nb15.csv',
         'normal_class': 'Benign',
         'label_col': 'Label',
+        'prefixspan_col': 'sbytes',
         'features': ['dur', 'sbytes', 'dbytes', 'sttl', 'dttl', 'sloss', 'dloss',
                                'Sload', 'Dload', 'Spkts', 'Dpkts', 'swin', 
                                'dwin', 'stcpb', 'dtcpb', 'smeansz', 'dmeansz',
@@ -146,7 +159,7 @@ def preprocessing(dataset_name, args):
         df = pd.read_csv(data_file_path, nrows=args.nrows)
     else:
         df = pd.read_csv(data_file_path)
-    
+
     # reduce memort consumption
     df = reduce_mem_usage(df)
 
@@ -154,19 +167,25 @@ def preprocessing(dataset_name, args):
     print(f"--- Converting Target to int dtype ---")
     df = convert_target_to_int(df, label_col, normal_class)
 
+    if 'time_col' in DATASET_DATA_FILE[dataset_name]:    
+        df[DATASET_DATA_FILE[dataset_name]["time_col"]] = pd.to_datetime(df[DATASET_DATA_FILE[dataset_name]["time_col"]])
+        df = df.sort_values(by=DATASET_DATA_FILE[dataset_name]["time_col"]).reset_index(drop=True)
+
     # splitting to train and test
     print(f"--- Train-Test Split ---")
+    # train_indexes, test_indexes = anomaly_detection_train_test_split(df, DATASET_DATA_FILE['ids17']['label_col'], window_size)
+    
     train_df, test_df = train_test_split(df, test_ratio)
-
     # filter out anomalous instances from training set
     print(f"--- Filtering out anomalous samples from training set ---")
     train_df = train_df[train_df[label_col] == 0]
 
     # rolling the dataset to obtain statistics every 5 'ticks'
     print(f'--- Rolling training set ---')
-    train_df = roll_dataset(train_df, features, label_col, window_size)
+    print(DATASET_DATA_FILE[dataset_name]['prefixspan_col'])
+    train_df = roll_dataset(train_df, features, label_col, DATASET_DATA_FILE[dataset_name]['prefixspan_col'], window_size, isnorm=True)
     print(f'--- Rolling test set ---')
-    test_df = roll_dataset(test_df, features, label_col, window_size)
+    test_df = roll_dataset(test_df, features, label_col, DATASET_DATA_FILE[dataset_name]['prefixspan_col'], window_size, isnorm=False)
 
     # normalizing
     print(f'--- Normalizing training set ---')
@@ -205,6 +224,42 @@ def convert_target_to_int(df, label_col, normal_class):
     
     return df
 
+
+def anomaly_detection_train_test_split(df, label_col, window_size):
+    # creating labels
+    label_col_idx = df.columns.get_loc(label_col)
+    rolled_df = np.array([df_.values for df_ in df.rolling(window_size) if df_.values.shape[0] == window_size])
+    # rolled_labels = df[label_col].rolling(window_size).max()[::window_size][1:]
+
+
+    # shape: (num_samples, window_size)
+    rolled_labels_channel  = np.squeeze(rolled_df[:, label_col_idx, :], axis=1)
+    # shape: (num_samples, 1)
+    rolled_labels = np.max(rolled_labels_channel, axis=1)
+
+
+    anomaly_samples : pd.Series = rolled_df[:, ] [rolled_labels == 1]
+    normal_samples : pd.Series = rolled_df[rolled_labels == 0]
+
+    print("normal samples: ", len(normal_samples))
+    print("anomaly samples: ", len(anomaly_samples))
+
+    anomaly_samples_num = min(50, len(anomaly_samples))
+    normal_test_sample_num = 3 * anomaly_samples_num
+    normal_train_samples_num = min(500, len(normal_samples) - normal_test_sample_num)
+    
+    anomaly_samples = anomaly_samples.sample(n=anomaly_samples_num, random_state=42)
+    normal_train_samples = normal_samples.sample(n=normal_train_samples_num, random_state=42)
+    normal_test_samples = normal_samples[~normal_samples.index.isin(normal_train_samples)].sample(n=normal_test_sample_num, random_state=42)
+
+    train_indexes = normal_train_samples.index.tolist()
+    test_indexes = anomaly_samples.index.tolist() + normal_test_samples.index.tolist()
+
+    train_mat = []
+    test_mat = []
+
+    return train_indexes, test_indexes
+
 def train_test_split(df, test_ratio):
     """
     Splitting the given dataset to train and test by preserving the order between the samples (it is time series data).
@@ -223,7 +278,7 @@ def train_test_split(df, test_ratio):
     
     return train_df, test_df
 
-def roll_dataset(df, features, label_col, window_size):
+def roll_dataset(df, features, label_col, prefixspan_col, window_size, isnorm):
     """
     Convolve a window over the given data and extact statistics (min, max, std).
 
@@ -239,18 +294,89 @@ def roll_dataset(df, features, label_col, window_size):
     df_label = df[label_col]
 
     # sliding the window
-    rolled_max = df_features.rolling(window_size).max()
-    rolled_max.columns = [f'MAX - {column}' for column in list(rolled_max.columns)]
 
-    rolled_min = df_features.rolling(window_size).min()
-    rolled_min.columns = [f'MIN - {column}' for column in list(rolled_min.columns)]
+    # MAX features
+    rolled_max = df_features.rolling(window_size).max()[window_size+1:]
+    if isnorm:
+        rolled_max = rolled_max[::window_size]
+    rolled_max.columns = [f'MAX_{column}' for column in list(rolled_max.columns)]
+    
+    # wavelet features
+    def perform_dwt(df_):
+        return pywt.dwt(df_, 'db1')[0][:, 0].tolist()
+    rolled_pywt = [perform_dwt(df_) for df_ in tqdm(df_features.rolling(window_size), total=len(df_features), desc="Wavelets")]
+    rolled_pywt_df = pd.DataFrame({'all_dwts': rolled_pywt})
+    rolled_pywt_df = pd.DataFrame(rolled_pywt_df['all_dwts'].tolist(), rolled_pywt_df.index)[window_size+1:]
+    if isnorm:
+        rolled_pywt_df = rolled_pywt_df[::window_size]
+    rolled_pywt_df.columns = [f'Wavelet_{i}' for i in range(len(rolled_pywt_df.columns))] # type: ignore
 
-    rolled_std = df_features.rolling(window_size).std()
-    rolled_std.columns = [f'STD - {column}' for column in list(rolled_std.columns)]
+    # # Arima features
+    # print("Arima Features:")
+    # rolled_arima = df_features.rolling(window_size).agg(lambda signal: ARIMA(signal, order=(1, 1, 2)).fit().forecast(1))[::window_size][1:]
+    # rolled_arima.columns = [f'ARIMA_forecast_{column}' for column in list(rolled_arima.columns)]
 
-    rolled_labels = df_label.rolling(window_size).max()
+    # PrefixSpan features
+    print("PrefixSpan Features")
+    db = [df_[prefixspan_col].tolist() for df_ in tqdm(df_features.rolling(window_size), total=len(df_features), desc="Construct Sequence DB") if (np.isinf(df_[prefixspan_col]).sum()==0)]
+    db = np.array(db[window_size+1:])
 
-    rolled = pd.concat([rolled_max, rolled_min, rolled_std, rolled_labels], axis=1)
+    # remove NaNs from sequences
+    db = db[np.isnan(db).sum(axis=1) == 0]
+
+    sax = SymbolicAggregateApproximation(n_bins=3, strategy='normal')
+    print("Performin SAX")
+    X_sax = sax.fit_transform(db)
+
+    print("PrefixSpan Mining")
+    ps = PrefixSpan(X_sax)
+    ps.minlen = 3
+    min_support = int(0.002*len(X_sax))
+    TIRPS = [tirp[1] for tirp in sorted(ps.frequent(minsup=min_support, closed=True, generator=True), key=lambda tirp: tirp[0], reverse=True)[0:5]]
+    
+
+    print(f"PrefixSpan TIRPS: {TIRPS}")
+
+
+    def prefixspan_extraction(series_):
+        series_ = series_.tolist()
+        prefixspan_features  = [0] * len(TIRPS) 
+        for tirp_i in range(len(TIRPS)):
+            tirp = TIRPS[tirp_i]
+            for i in range(len(series_) - len(tirp) + 1 ):
+                subseq = series_[i:i+len(tirp)]
+                if tirp == subseq:
+                    prefixspan_features[tirp_i] = 1
+                    break
+
+        return prefixspan_features
+
+    rolled_prefixspan = [prefixspan_extraction(i) for i in tqdm(X_sax, desc='TIRPS Extraction', total=len(X_sax))]
+    rolled_prefixspan_df = pd.DataFrame({'all_prefixspan': rolled_prefixspan})
+    rolled_prefixspan_df = pd.DataFrame(rolled_prefixspan_df['all_prefixspan'].tolist(), rolled_prefixspan_df.index)
+    if isnorm:
+        rolled_prefixspan_df = rolled_prefixspan_df[::window_size]
+    rolled_prefixspan_df.columns = [f'TIRP_{i}' for i in range(len(rolled_prefixspan_df.columns))] # type: ignore
+
+    # MIN features
+    rolled_min = df_features.rolling(window_size).min()[window_size+1:]
+    if isnorm:
+        rolled_min = rolled_min[::window_size]
+    rolled_min.columns = [f'MIN_{column}' for column in list(rolled_min.columns)]
+
+    # STD features
+    rolled_std = df_features.rolling(window_size).std()[window_size+1:]
+    if isnorm:
+        rolled_std = rolled_std[::window_size]
+    rolled_std.columns = [f'STD_{column}' for column in list(rolled_std.columns)]
+
+    # creating labels
+    rolled_labels = df_label.rolling(window_size).max()[window_size+1:]
+    if isnorm:
+        rolled_labels = rolled_labels[::window_size]
+    rolled = pd.concat([rolled_max, rolled_min, rolled_std, rolled_labels, rolled_pywt_df, rolled_prefixspan_df], axis=1)
+
+    # rolled = rolled[window_size:]
     rolled = rolled.fillna(rolled.mean())
     rolled[label_col] = rolled[label_col].astype(np.int16)
 
